952005c63bfcd33b743e719ff89747aaacc4a355
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-19 22:51:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
 	var form=document.getElementById("form1");
-	var e1=form.elements[fname].value;
-	var e2=form.elements[lname].value;
+	var e1=form.elements['fname'].value;
+	var e2=form.elements['lname'].value;
 	alert(e1+'\n'+e2);
 }
